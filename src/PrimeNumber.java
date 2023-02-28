451a0692b37a1303893ade807e75ed2aef0f9ac0
Michael <mblakey2@ggc.edu>
2023-02-28 00:46:04 -0500

    add PrimeNumber java

---
+++ src/PrimeNumber.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[]args){
+        Scanner in = new Scanner (System.in);
+        System.out.print("Enter a number between 1 and 100:");
+        int number = in.nextInt();
+       boolean prime = number > 1;
+       for(int n = 2; n < number; n++){
+           if (number % n == 0){
+               prime = false;
+               }
+           }
+        if (prime){
+            System.out.println( number + " is a prime number.");
+        }else{
+            System.out.println( number + " number is not a prime number.");
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
